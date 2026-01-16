ec189049bb031efc85edfbcbfa1cc9c67071b6a5
karlieward <karlieward3@gmail.com>
2026-01-15 17:19:29 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Mission2;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.Write("How many dice rolls would you like to simulate? ");
+        int numRolls = int.Parse(Console.ReadLine());
+
+        RollDice roller = new RollDice();
+        int[] counts = roller.SimulateRolls(numRolls);
+        
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS: ");
+        System.Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        System.Console.WriteLine($"Total number of rolls = {numRolls}");    
+        System.Console.WriteLine();
+
+        for (int sum = 2; sum <= 12; sum++)
+        {
+            int thisSum = counts[sum];
+            double percentage = (double)thisSum / numRolls * 100;
+            int starsCount = (int)Math.Round(percentage);
+            string stars = new string('*', starsCount);
+            
+            System.Console.WriteLine($"{sum}: {stars}");
+            System.Console.WriteLine();
+        }
+        
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,19 @@
+namespace Mission2;
+
+public class RollDice
+{
+    public int[] SimulateRolls(int numRolls)
+    {
+        Random rnd = new Random();
+        int[] counts = new int[13];
+        for (int i = 0; i < numRolls; i++)
+        {
+            int die1 = rnd.Next(1, 7);
+            int die2 = rnd.Next(1, 7);
+            int sum = die1 + die2;
+
+            counts[sum] = counts[sum] + 1;
+        }
+        return counts;
+    }
+}
